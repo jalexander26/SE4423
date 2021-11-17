a2b75be6ecf0087ab76fea2f7d2a9e5211fe9b7e
unknown <Jalexander26@uco.edu>
2021-11-16 20:25:27 -0600

    Start

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println(“A bug is fixed")
+		System.out.println(" on bug_fixing branch!”);
+	}
+
+}
